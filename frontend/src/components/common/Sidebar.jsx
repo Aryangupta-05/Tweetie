e0f9f4cc7dbad69bc1950f375f10fb9f259c0e7d
Aryan <aryangupta050903@gmail.com>
2025-09-12 03:55:56 +0530

    update Sidebar.jsx

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -76,26 +76,29 @@ const Sidebar = () => {
 					</li>
 				</ul>
 				{authUser && (
-					<div className='mt-auto mb-10 flex gap-2 items-start transition-all duration-300 hover:bg-[#181818] py-2 px-4 rounded-full'>
-						<Link
-							to={`/profile/${authUser.username}`}
-							className='flex gap-2 items-start flex-1'
-						>
-							<div className='avatar hidden md:inline-flex'>
-								<div className='w-8 rounded-full'>
-									<img src={authUser?.profileImg || "/avatar-placeholder.png"} />
-								</div>
+					<Link
+						to={`/profile/${authUser.username}`}
+						className='mt-auto mb-10 flex gap-2 items-start transition-all duration-300 hover:bg-[#181818] py-2 px-4 rounded-full'
+					>
+						<div className='avatar hidden md:inline-flex'>
+							<div className='w-8 rounded-full'>
+								<img src={authUser?.profileImg || "/avatar-placeholder.png"} />
 							</div>
+						</div>
+						<div className='flex justify-between flex-1'>
 							<div className='hidden md:block'>
 								<p className='text-white font-bold text-sm w-20 truncate'>{authUser?.fullName}</p>
 								<p className='text-slate-500 text-sm'>@{authUser?.username}</p>
 							</div>
-						</Link>
-						<BiLogOut
-							className='w-5 h-5 cursor-pointer text-white hover:text-red-500'
-							onClick={() => logout()}
-						/>
-					</div>
+							<BiLogOut
+								className='w-5 h-5 cursor-pointer'
+								onClick={(e) => {
+									e.preventDefault();
+									logout();
+								}}
+							/>
+						</div>
+					</Link>
 				)}
 			</div>
 		</div>
